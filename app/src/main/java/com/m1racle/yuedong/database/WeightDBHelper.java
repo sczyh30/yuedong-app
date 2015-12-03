128c051f9ebce8bdea08254ec7577ceb29fb2272
Eric Zhao <791801973@qq.com>
2015-12-04 00:04:01 +0800

    update db components

---
+++ app/src/main/java/com/m1racle/yuedong/database/WeightDBHelper.java
@@ -0,0 +1,46 @@
+package com.m1racle.yuedong.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Weight Database Helper
+ * SQLite 3 support
+ * @author sczyh30
+ */
+public class WeightDBHelper extends SQLiteOpenHelper{
+
+    private static final String CREATE_SQL = "CREATE TABLE weight (" +
+            "weight float NOT NULL," +
+            "height float NOT NULL," +
+            "index float NOT NULL," +
+            "w_time date NOT NULL" +
+            "tip varchar(255) NOT NULL" +
+            ")";
+
+    private Context context;
+
+    public WeightDBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.beginTransaction();
+        try {
+            db.execSQL(CREATE_SQL);
+            db.setTransactionSuccessful();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        db.endTransaction();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // no need
+    }
+
+}
